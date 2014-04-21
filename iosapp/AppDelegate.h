bd122caf5bac4f33762c1c97ca36b8d81bf85ce5
ashwin sidharthan <ashwinsidharthan@ashwins-MacBook-Pro.local>
2014-04-21 17:38:13 +0200

    Initial Commit

---
+++ iosapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iosapp
+//
+//  Created by ashwin sidharthan on 21/04/14.
+//  Copyright (c) 2014 SkopMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iosapp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iosapp
+//
+//  Created by ashwin sidharthan on 21/04/14.
+//  Copyright (c) 2014 SkopMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
